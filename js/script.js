fa10239e7f91c2aa611914b90ad28d5f5f0838ad
Andrean Stephanus <andreanstephanus@gmail.com>
2024-07-02 19:57:08 +0700

    main

--- js/script.js
+++ js/script.js
@@ -10,3 +10,23 @@ document.addEventListener("DOMContentLoaded", function(){
 
   greetdiv.innerHTML = `Hi, ${Username}! Welcome to our website.`;
 });
+
+
+document.getElementById("Data").addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  var formData = new FormData(this);
+  var name = formData.get("name");
+  var tgg = formData.get("tgg");
+  var gender = formData.get('JenisKe');
+  var pesan = formData.get("Pesan");
+  var time = new Date().toLocaleString();
+  
+  var outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = `
+    <br>${time}
+    <p>Nama: ${name}<br>
+    Tanggal Lahir: ${tgg}<br>
+    Jenis Kelamin: ${gender}<br>
+    Pesan: ${pesan}</p>`
+});
