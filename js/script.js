126cb210618347a42129fa5c30ff77bf02bd763d
Andrean Stephanus <andreanstephanus@gmail.com>
2024-07-04 16:48:41 +0700

    slide mobile

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(){
     Username = nama_default;
   }
 
-  greetdiv.innerHTML = `Hi, ${Username}! Welcome to our website.`;
+  greetdiv.innerHTML = `Hi, ${Username}.`;
 });
 
 
@@ -29,4 +29,81 @@ document.getElementById("Data").addEventListener('submit', function(event) {
     Tanggal Lahir: ${tgg}<br>
     Jenis Kelamin: ${gender}<br>
     Pesan: ${pesan}</p>`
+});
+
+function scrollToSec(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const slides = document.querySelector('.Slides');
+  const slide = document.querySelectorAll(".slide");
+  const Jum_slide = slide.length;
+  let cur_I = 0;
+  let Start_X = 0;
+  let End_X = 0;
+  let Interval;
+
+
+  function update_pos() {
+      slides.style.transform = `translateX(-${cur_I * 100}%)`;
+  }
+
+ 
+  function shownext() {
+      cur_I = (cur_I + 1) % Jum_slide;
+      update_pos();
+  }
+
+
+  function showprev() {
+      cur_I = (cur_I - 1 + Jum_slide) % Jum_slide;
+      update_pos();
+  }
+
+  function startAuto() {
+      Interval = setInterval(shownext, 10000);
+  }
+
+
+  function stopAuto() {
+      clearInterval(Interval);
+  }
+
+
+  slides.addEventListener("touchstart", function(event) {
+      Start_X = event.touches[0].clientX;
+      stopAuto(); 
+  });
+
+  slides.addEventListener("touchend", function(event) {
+      End_X = event.changedTouches[0].clientX;
+      if (Start_X > End_X + 50) { 
+          shownext();
+      } else if (Start_X < End_X - 50) { 
+          showprev();
+      }
+      startAuto(); 
+  });
+
+  document.querySelector('.Slide_but_left').addEventListener('click', function() {
+      showprev();
+      stopAuto(); 
+      startAuto(); 
+  });
+
+  document.querySelector('.Slide_but_right').addEventListener('click', function() {
+      shownext();
+      stopAuto(); 
+      startAuto(); 
+  });
+
+  update_pos();
+  startAuto();
+
+
+  window.addEventListener('resize', update_pos);
 });
